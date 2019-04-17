90a5ac5fce7236ed1041a2b60bccebaac4e1642d
MohammedAlfouzan <47580577+MohammedAlfouzan@users.noreply.github.com>
2019-04-17 14:06:29 -0700

    Add files via upload

--- User_SignsUp.php
+++ User_SignsUp.php
@@ -1,65 +1,52 @@
-<?php
-
+<?php 
 include 'utility.php';
 
-class User_login
+class User_SignsUp 
 {
 
-$name_error;
-$password_error;
-
-  
+public $name = $_POST["name"];
+public $email = $_POST["email"];
+public $emailC = $_POST["emailC"];
+public $password = $_POST["password"];
+public $passwordC = $_POST["passwordC"];
+ 
   function EmptyBoxes()
   {
-  	if(empty($_POST["name"]) || empty($_POST["password"]))
+  	if(empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["emailC"]) || 
+  					empty($_POST["password"]) || empty($_POST["passwordC"]))
   	{
   		$name_error = "Please fill out the empty box/boxes";
   		return false;
  	}
  	return true;
- 	
   }
- 
- function nameExisting($name)
- {
- 	$query = queryDatabase('profile_tbl','name',"WHERE name=$name", 1);
- 	if($name != $query)
- 	{
- 		$name_error = "User not found";
- 		return false;
- 	}
- 	return true
- 	
- }
- 
- function passwordExisting($password)
- {
- 	$query = queryDatabase('profile_tbl','password',"WHERE password=$password", 1);
- 	if ($password != $query)
- 	{
- 		$password_error = "password not correct";
- 		return false;
- 	}
- 	return true;
-	
- }
- 
- function login($name, $pasword)
- {
- 	if (nameExisting($name) == true && passwordExisting($password) == true)
- 	{
- 		
- 	} 
- 	
- 	
- 		
-
- 		
- 		
   
+  function checkMatchingEmails()
+  {
+  	return checkExisting($email, $emailC);
+  }
   
+  function checkMatchingPasswords($password, $passwordC)
+  {
+  	return checkExisting($password, $passwordC);
+  }
+    
+  function checkExistingName()
+  {
+   $query = queryDatabase('profile_tbl','name',"WHERE name=$name", 1);
+   return checkExisting($name, $query);
+  }
   
-  
+  function checkAndInsert()
+  {
+  if (checkExistingName() == true && checkMatchingPasswords() == true
+  	 && checkMatchingEmails() == true && EmptyBoxes() == true)
+	{
+ 	insertInto("profile_tbl", "name", $name);
+  	insertInto("profile_tbl", "email", $email);
+  	insertInto("profile_tbl", "password", $password);
+  	}
+  }
 }
-  
-  
+
+?>
